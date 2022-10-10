1534e670613cfa126bc593829dfb3be6c517026c
Lucas Moletta <amiralezer@gmail.com>
2022-10-10 20:47:04 -0300

    second commit
    table being loaded from db.
    alchemy models fixed.

--- application/routes.py
+++ application/routes.py
@@ -1,11 +1,12 @@
 from application import app
 from flask import render_template, request, json, Response, redirect, flash, url_for, session
-#from application.models import User, Course, Enrollment
+from application.models import dimMedication
 #from application.forms import LoginForm, RegisterForm
+import psycopg2
 
 
 @app.route("/")
-@app.route("/index")
-@app.route("/home")
 def index():
-    return render_template("index.html", index=True )
+    
+    medications = dimMedication.query.all()
+    return render_template("courses.html", index=True, courseData=medications)
